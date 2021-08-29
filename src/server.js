d880284ae8e64a662b45cf32dbdb575efa3dc5d2
byuengyun Chae <chaeby7@gmail.com>
2021-08-29 21:39:11 +0900

    Update wetube

--- src/server.js
+++ src/server.js
@@ -2,15 +2,20 @@ import express from "express";
 const Port = 4000;
 const app = express();
 
-const gossipMiddleWare = (req, res, next) => {
-    console.log("Steven Gerrard");
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
     next();
 }
 
-const getInToServer =  (req, res) => {
-    return res.send ("We go norich");
-}
-app.get("/", gossipMiddleWare ,getInToServer);
 
-const  handleCheck = () =>  console.log(`Server listening on port http://localhost:${Port}`);
-app.listen(Port,handleCheck);
+
+const handleHome =  (req, res) => {
+    return res.send ("I love middlewares");
+}
+app.use(logger);
+app.get("/",handleHome);
+
+const handleListening = () =>  {
+    console.log(`Server listening on port http://localhost:${Port}`);
+}
+app.listen(Port,handleListening);
