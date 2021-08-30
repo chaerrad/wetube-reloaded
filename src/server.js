0411d856a04cf92e99d4f160394b251c5a1df855
byuengyun Chae <chaeby7@gmail.com>
2021-08-30 13:49:35 +0900

    Update wetube Routers

--- src/server.js
+++ src/server.js
@@ -1,33 +1,40 @@
 import express from "express";
+import morgan from "morgan";
+
 const Port = 4000;
+
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
-
-const privateMiddleware = (req,res,next) => {
-    const url = req.url;
-    if(url === "/protected")
-    {   
-        return res.send("<h1>Not Allowed</h1>");
-    }
-    console.log("Allowed you may continue");
-    next();
-}
-
-const handleHome =  (req, res) => {
-    return res.send ("I love middlewares");
-}
-
-const handleProtected = (req, res) => {
-    return res.send("Welcome to the private lounge");
-}
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/",handleHome);
-app.get("/protected",handleProtected);
+
+const globalRouter = express.Router();
+
+const handleHome = (req,res) => {
+    res.send("Home");
+}
+
+globalRouter.get("/",handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req,res) => {
+    res.send("Edit User");
+}
+
+userRouter.get("/edit",handleEditUser)
+
+const videoRouter = express.Router();
+
+const handleWatchVideos = (req,res) => {
+    res.send("Watch Videos");
+}
+videoRouter.get("/watch",handleWatchVideos);
+
+app.use("/", globalRouter);
+app.use("/users",userRouter);
+app.use("/videos", videoRouter);
+
 
 const handleListening = () =>  {
     console.log(`Server listening on port http://localhost:${Port}`);
