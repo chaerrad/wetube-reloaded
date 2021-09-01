5e69a8054f56f4f9d7733a621a7f3e07c32762bf
byuengyun Chae <chaeby7@gmail.com>
2021-09-01 14:19:45 +0900

    Update wetube partials <inheritance and block>

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,15 +1,11 @@
 export const trending = (req, res) => res.render("home");
 
 export const see = (req,res) => {
-    return res.send(
-        `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Watch video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
-      );
+    return res.render("watch" );
 }
 
 export const edit = (req,res) => {
-    return res.send(
-        `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Edit video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
-      );
+    res.render("edit");
 }
 export const search = (req,res) => {
     res.send("Search");
