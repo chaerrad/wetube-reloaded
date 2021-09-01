be8a76ef87d174cd63919cda45ad4e9a758674a0
byuengyun Chae <chaeby7@gmail.com>
2021-09-01 13:46:22 +0900

    Update wetube using pug

--- src/server.js
+++ src/server.js
@@ -7,11 +7,14 @@ import videoRouter from "./routers/videoRouter.js";
 
 const Port = 4000;
 
+console.log(process.cwd());
+
 const app = express();
 const logger = morgan("dev");
 
+app.set("view engine","pug");
+app.set("views", process.cwd()+"/src/views");
 app.use(logger);
-
 app.use("/", globalRouter);
 app.use("/users",userRouter);
 app.use("/videos", videoRouter);
