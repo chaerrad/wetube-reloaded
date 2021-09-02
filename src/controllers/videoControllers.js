537a93e53110f3c91b79653a9328e4732a66ee83
byuengyun Chae <chaeby7@gmail.com>
2021-09-02 14:31:09 +0900

    partial integer and mixins

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,4 +1,34 @@
-export const trending = (req, res) => res.render("home",{pageTitle: "Home"});
+const videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        commets: 2,
+        createdAt: "2 minutes age",
+        view: 59,
+        id:1
+    },
+    {
+        title: "Second Video",
+        rating: 4,
+        commets: 3,
+        createdAt: "6 minutes age",
+        view: 69,
+        id:2
+    },
+    {
+        title: "First Video",
+        rating: 3,
+        commets: 11,
+        createdAt: "20 minutes age",
+        view: 79,
+        id:3
+    }
+];
+
+export const trending = (req, res) => 
+{
+    return res.render("home",{pageTitle: "Home",videos});
+}
 
 export const see = (req,res) => {
     return res.render("watch" );
