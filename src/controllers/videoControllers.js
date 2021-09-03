0448b8b068469f9801cb3e816cd9ccb4932bfc4f
byuengyun Chae <chaeby7@gmail.com>
2021-09-03 15:44:40 +0900

    Update wetube recap

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -28,22 +28,30 @@ const videos = [
 export const trending = (req, res) => 
 {
     return res.render("home",{pageTitle: "Home",videos});
-}
+};
 
 export const watch = (req,res) => {
     const id = req.params.id;
     const video= videos[id-1]
     return res.render("watch",{ pageTitle: `Watching: ${video.title}`, video} );
-}
+};
 
 export const getEdit = (req,res) => {
     const id = req.params.id;
     const video= videos[id-1]
     return res.render("edit",{pageTitle:`Editing: ${video.title}`, video} );
-}
+};
 export const postEdit = (req,res) => {
     const id = req.params.id;
     const title = req.body.title;
 
     return res.redirect(`/videos/${id}`);
+};
+
+export const getUpLoad = (req,res) => {
+    return res.render("upload",{ pageTitle: "Upload Video" });
+};
+
+export const postUpLoad = (req,res) => {
+    return res.redirect("/");
 }
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {watch,upload,deleteVideo,getEdit,postEdit} from "../controllers/videoControllers.js";
+import {watch,trending,getEdit,postEdit,getUpLoad,postUpLoad} from "../controllers/videoControllers.js";
 
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)",watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpLoad).post(postUpLoad);
 
 export default videoRouter;
