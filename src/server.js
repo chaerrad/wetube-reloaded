2b9f9cbfd07ce98aaa94ca8b5b41d6ba9485d0ff
byuengyun Chae <chaeby7@gmail.com>
2021-12-30 20:13:05 +0900

    Update wetube Session and Cookies part One

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import globalRouter from "./routers/rootRouter.js";
 import userRouter from "./routers/userRouter.js";
 import videoRouter from "./routers/videoRouter.js";
@@ -12,6 +13,22 @@ app.set("view engine","pug");
 app.set("views", process.cwd()+"/src/views");
 app.use(logger);
 app.use(express.urlencoded({extended: true}));
+
+app.use(
+    session({
+        secret: "Hello",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+app.use((req, res, next) => {
+    req.sessionStore.all((error,sessions) => {
+        console.log(sessions);
+        next();
+    });
+});
+
 app.use("/", globalRouter);
 app.use("/users",userRouter);
 app.use("/videos", videoRouter);
