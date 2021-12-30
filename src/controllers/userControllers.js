a1463d8bad2e49b68125a9c2e4d640e664428df5
byuengyun Chae <chaeby7@gmail.com>
2021-12-30 14:40:38 +0900

    Login part two

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 export const getJoin = (req,res) => {
     res.render("join",{pageTitle: "Join"});
 }
@@ -43,12 +44,18 @@ export const getLogin = (req,res) => {
 }
 export const postLogin = async (req,res) => {
     const {username,password}=req.body;
-    const exists = await User.exists({username});
-    if(!exists)
+    const pageTitle = "login";
+    const user = await User.findOne({ username });
+    if(!user)
     {
-        return res.status(400).render("login",{pageTitle:"Login",errorMessage:"An account with this username doesn't exists."});
+        return res.status(400).render("login",{pageTitle ,errorMessage:"An account with this username doesn't exists."});
     }
-    res.end();
+    const ok = await bcrypt.compare(password,user.password);
+    if (!ok)
+    {
+        return res.status(400).render("login",{pageTitle ,errorMessage:"Wrong password"});
+    }
+    res.redirect("/");
 }
 export const logout = (req,res) => {
     res.send("Logout Users");
