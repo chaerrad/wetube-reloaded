522275993bf9d789c21b7161671f618f06298fd9
byuengyun Chae <chaeby7@gmail.com>
2022-01-05 17:14:43 +0900

    Update wetube Github Login

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,16 @@
 import express from "express";
-import { Edit, remove, logout, see, startGithubLogin, finishGithubLogin } from "../controllers/userControllers.js";
+import {
+  edit,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userControllers";
 const userRouter = express.Router();
 
-userRouter.get("/logout",logout);
-userRouter.get("/edit",Edit);
-userRouter.get("/remove",remove);
-userRouter.get("/github/start",startGithubLogin)
-userRouter.get("/github/finish",finishGithubLogin)
-userRouter.get(":id",see);
-
+userRouter.get("/logout", logout);
+userRouter.get("/edit", edit);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get(":id", see);
 export default userRouter;
