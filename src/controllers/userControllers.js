a38b8f823642dfe76c39413b3173d6161b59ce21
byuengyun Chae <chaeby7@gmail.com>
2022-01-11 19:47:28 +0900

    Update wetube file uploads part one

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -139,7 +139,9 @@ export const postEdit = async (req, res) => {
       user: { _id },
     },
     body: { name, email, username, location },
+    file,
   } = req;
+  console.log(file);
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
--- src/middlewares.js
+++ src/middlewares.js
@@ -1,3 +1,4 @@
+import multer from "multer";
 export const localsMiddleware = (req,res,next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
     res.locals.siteName = "Wetube";
@@ -13,12 +14,14 @@ export const protectorMiddleware = (req, res, next) => {
     }
   };
   
-  export const publicOnlyMiddleware = (req, res, next) => {
+  export const 
+  publicOnlyMiddleware = (req, res, next) => {
     if (!req.session.loggedIn) {
       return next();
     } else {
       return res.redirect("/");
     }
   };
+  export const uploadFiles = multer({ dest: "uploads/" });
 
 
